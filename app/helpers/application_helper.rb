10bb4dd5cb95acf5f37be891161c9ba893a99dd6
Jose Padilla <jose.padilla.88@gmail.com>
2012-06-03 23:24:56 -0700

    Added title helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#Returns whether there is a costume page title.			#Comment or Documentation
+	def full_title(page_title)								#Method definition
+		base_title = "Obscura"									#Variable definition
+		if page_title.empty?									#Boolean test
+			base_title												#Implicit return
+		else
+			"#{base_title} | #{page_title}"							#String interpolation
+		end														#End of Boolean test
+	end 													#End of method definition
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,12 +12,17 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Welcome!')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title 'Obscura'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} | Home")
+      page.should have_selector('title', :text => 'Obscura')
+    end
+
+    it "should not have a costume title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
-  
+
   describe "Help page" do
 
   	it "should have the h1 'Help Page'" do
