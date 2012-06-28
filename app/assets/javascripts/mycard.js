4719d3bbee18efc0a24a70ebd3290f6004607b46
Jose Padilla <jose.padilla.88@gmail.com>
2012-06-27 23:13:32 -0700

    Finished the card animation.

---
+++ app/assets/javascripts/mycard.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.flip').click(function(){
+        $(this).find('.card').addClass('flipped').mouseleave(function(){
+            $(this).removeClass('flipped');
+        });
+        return false;
+    });
+ });
